11aaf542f3e3f120a8a7872e50f2a4d761b9e410
wangganyi <wangganyi@egova.com.cn>
2020-05-19 17:18:48 +0800

    [A]添加seata client端的配置文件file.conf & 向Transaction Coordinator注册的配置文件

---
+++ eureka/src/main/java/cn/com/k1oud/eureka/Test.java
@@ -0,0 +1,17 @@
+package cn.com.k1oud.eureka;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Test {
+    //@Bean
+    //@ConfigurationProperties("server.port")
+    String destStr;
+
+
+
+}
+
+
